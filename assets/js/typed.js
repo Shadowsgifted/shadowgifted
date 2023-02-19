803a888fea534af3ae6b5fdeff3c37610f983a5d
Shadowsgifted <125776720+Shadowsgifted@users.noreply.github.com>
2023-02-18 21:12:55 -0500

    Add files via upload

---
+++ assets/js/typed.js
@@ -0,0 +1,7 @@
+new Typed('#typed',{
+  strings : [,'shadows#1234','/prediction','/varity','immortal','best dev','crypto mastermind','paster',],
+  typeSpeed : 40,
+  delaySpeed : 90,
+  showCursor: true,
+  loop : true
+})
